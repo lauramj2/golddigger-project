2c535ea1dcf2b4520aab941f07c7bc4f19dc3e9e
lauramj2 <lauraamarie2@gmail.com>
2026-01-19 11:28:31 -0500

    reorganized files

---
+++ server.js
@@ -0,0 +1,24 @@
+import http from "node:http"
+import { serveStatic } from "./utils/serveStatic.js"
+import { handleGet, handlePost } from "./handlers/routeHandlers.js"
+
+const PORT = 3000
+
+const __dirname = import.meta.dirname
+
+const server = http.createServer(async (req, res) => {
+    
+    if (req.url === "/api") {
+        if (req.method === "GET") {
+            return await handleGet(res)
+        }
+        else if (req.method === "POST") {
+            return await handlePost(req, res)
+        }
+    }
+    else if (!req.url.startsWith === "/api") {
+        return await serveStatic(req, res, __dirname)
+    }
+})
+
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
